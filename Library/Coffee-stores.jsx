694d6533ade9c81113cabcfb87741ffca0f15513
Vedant Bhavsar <Vedantbhavsar67@gmail.com>
2023-07-06 01:33:20 +0530

    Deploy

--- Library/Coffee-stores.jsx
+++ Library/Coffee-stores.jsx
@@ -15,10 +15,13 @@ const GetCoffeeStoreImages = async (limit, query, size = "regular") => {
 };
 const FetchCoffeeStores = async (
   ll = "23.039907727550556%2C72.53117613134259",
-  limit = 5
+  limit = 20,
 ) => {
+  try {
+    
+  
   const res = await fetch(
-    `https://api.foursquare.com/v3/places/nearby?ll=${ll}&query=&limit=${limit}`,
+    `https://api.foursquare.com/v3/places/nearby?ll=${ll}&query=&limit=${limit}&radius=25000`,
     {
       method: "GET",
       headers: {
@@ -31,15 +34,19 @@ const FetchCoffeeStores = async (
   const StoreData = Data.results.map(async (res, i) => {
    
     const photos = await GetCoffeeStoreImages(limit,res.name);
+const pic = photos.length > 0 ? await photos[0] : "/static/NotFound.png"
     return {
       name: res.name,
       id: res.fsq_id,
       address: res.location.formatted_address,
-      ImgUrl: photos.length > 0 ? photos[0] : "/static/NotFound.png",
+      ImgUrl: pic,
+      // ImgUrl: "/static/NotFound.png",
        };
   });
-
   return await Promise.all(StoreData);
+} catch (error) {
+    console.error(error);
+  }
 };
 
 export default FetchCoffeeStores;
