42ca9a1c7b66a6bcee854fea135b54f6bab0590f
Vedant Bhavsar <Vedantbhavsar67@gmail.com>
2023-07-06 02:43:25 +0530

    Tailwind setup

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,6 @@
 import StoreProvider from "@/store/store-context";
 import "@/styles/globals.css";
+import "@/dist/output.css";
 
 export default function App({ Component, pageProps }) {
   return (
